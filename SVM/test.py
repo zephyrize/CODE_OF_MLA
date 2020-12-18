cd0b342177a1030d4622186ad9253479cab336f7
sophia-seu <askitachi@163.com>
2020-12-18 23:41:55 +0800

    删除SVM

--- SVM/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import SMO
-
-file_name = "testSet.txt"
-train_data, labels = SMO.get_data(file_name)
-data= SMO.start(train_data, labels, 0.6, 100)
-data.w = SMO.cal_w(data)
-SMO.plot_data(data)
